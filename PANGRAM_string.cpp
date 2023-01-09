0e976429d2673d7fc64888dea8234b3068ca1b42
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2023-01-10 00:49:44 +0530

    Create PANGRAM_string.cpp

---
+++ PANGRAM_string.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+string pangrams(string s) {
+    int arr[26] = {0};
+    for (char & c :s) {
+        if (c >= 97 && c<= 122)
+        arr[c-97] = 1;
+        else if (c >=65 && c <=90)
+        arr[c-65] = 1;
+        
+    }
+    for (int i = 0; i<26; i++)
+        if (arr[i] == 0) 
+        return "not pangram";
+    return "pangram";
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
